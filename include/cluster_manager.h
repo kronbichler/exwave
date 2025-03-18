14e7270d0c76255a09f21d37b499cc07855f1880
Martin Kronbichler <martin.kronbichler@rub.de>
2025-03-18 10:18:54 +0100

    Indent with clang-format

--- include/cluster_manager.h
+++ include/cluster_manager.h
@@ -17,7 +17,9 @@
 #define cluster_manager_h_
 
 #include <deal.II/distributed/tria.h>
+
 #include <deal.II/dofs/dof_handler.h>
+
 #include <deal.II/lac/la_parallel_vector.h>
 
 namespace HDG_WE
@@ -28,99 +30,128 @@ namespace HDG_WE
   class ClusterManager
   {
   public:
-
     static const unsigned int n_vect = VectorizedArray<Number>::size();
 
-    ClusterManager(const double rel_tol=1.e-4)
-      :relative_tolerance(rel_tol)
+    ClusterManager(const double rel_tol = 1.e-4)
+      : relative_tolerance(rel_tol)
     {}
 
-    template <typename Operator> void perform_time_step(const Operator &op,
-                                                        const LinearAlgebra::distributed::Vector<Number>  &src,
-                                                        LinearAlgebra::distributed::Vector<Number>        &dst) const;
+    template <typename Operator>
+    void
+    perform_time_step(const Operator                                   &op,
+                      const LinearAlgebra::distributed::Vector<Number> &src,
+                      LinearAlgebra::distributed::Vector<Number> &dst) const;
 
-    template <int dim> void propose_cluster_categorization(std::vector<unsigned int> &categories,
-                                                           const Triangulation<dim> &tria,
-                                                           const std::vector<const DoFHandler<dim> *> &dof_handlers,
-                                                           const unsigned int max_clusters,
-                                                           const unsigned int max_diff,
-                                                           const double cfl_number);
+    template <int dim>
+    void
+    propose_cluster_categorization(
+      std::vector<unsigned int>                  &categories,
+      const Triangulation<dim>                   &tria,
+      const std::vector<const DoFHandler<dim> *> &dof_handlers,
+      const unsigned int                          max_clusters,
+      const unsigned int                          max_diff,
+      const double                                cfl_number);
 
-    template <int dim> void iterate_cluster_categorization(const Triangulation<dim> &tria,
-                                                           const std::vector<const DoFHandler<dim> *> &dof_handlers,
-                                                           const unsigned int max_clusters,
-                                                           const unsigned int max_diff,
-                                                           const double cfl_number);
+    template <int dim>
+    void
+    iterate_cluster_categorization(
+      const Triangulation<dim>                   &tria,
+      const std::vector<const DoFHandler<dim> *> &dof_handlers,
+      const unsigned int                          max_clusters,
+      const unsigned int                          max_diff,
+      const double                                cfl_number);
 
-    template <int dim> unsigned int cell_weight(const typename parallel::distributed::Triangulation<dim>::cell_iterator &cell,
-                                                const typename parallel::distributed::Triangulation<dim>::CellStatus)
+    template <int dim>
+    unsigned int
+    cell_weight(
+      const typename parallel::distributed::Triangulation<dim>::cell_iterator
+        &cell,
+      const typename parallel::distributed::Triangulation<dim>::CellStatus)
     {
-      const unsigned int cell_weight = element_categories[cell->active_cell_index()]/3;
+      const unsigned int cell_weight =
+        element_categories[cell->active_cell_index()] / 3;
 
       return cell_weight;
     }
 
-    template <typename Operator> void setup(const Operator &op);
+    template <typename Operator>
+    void
+    setup(const Operator &op);
 
-    template <typename Operator> void setup_mf_index_to_cell_index(const Operator &op);
+    template <typename Operator>
+    void
+    setup_mf_index_to_cell_index(const Operator &op);
 
     //{ routines for WaveEquationOperationADERLTS to ask for:
-    Number get_cell_time_step(unsigned int cell) const
+    Number
+    get_cell_time_step(unsigned int cell) const
     {
-      return cluster_timestepmultiples[cell_cluster_ids[cell]]*fastest_time_step;
+      return cluster_timestepmultiples[cell_cluster_ids[cell]] *
+             fastest_time_step;
     }
 
-    bool is_evaluate_cell(unsigned int cell) const
+    bool
+    is_evaluate_cell(unsigned int cell) const
     {
       return evaluate_cell[cell];
     }
 
-    bool is_update_cell(unsigned int cell) const
+    bool
+    is_update_cell(unsigned int cell) const
     {
       return update_cell[cell];
     }
 
-    bool is_evaluate_face(unsigned int face) const
+    bool
+    is_evaluate_face(unsigned int face) const
     {
       return evaluate_face[face];
     }
 
-    Number get_t1() const
+    Number
+    get_t1() const
     {
       return t1;
     }
 
-    Number get_t2() const
+    Number
+    get_t2() const
     {
       return t2;
     }
 
-    Number get_te(unsigned int cell) const
+    Number
+    get_te(unsigned int cell) const
     {
       return cell_timelevels[cell];
     }
 
-    Number get_dt() const
+    Number
+    get_dt() const
     {
       return dt;
     }
 
-    std::bitset<VectorizedArray<Number>::size()> get_phi_to_dst(unsigned int face) const
+    std::bitset<VectorizedArray<Number>::size()>
+    get_phi_to_dst(unsigned int face) const
     {
       return phi_to_dst[face];
     }
 
-    std::bitset<VectorizedArray<Number>::size()> get_phi_to_fluxmemory(unsigned int face) const
+    std::bitset<VectorizedArray<Number>::size()>
+    get_phi_to_fluxmemory(unsigned int face) const
     {
       return phi_to_fluxmemory[face];
     }
 
-    std::bitset<VectorizedArray<Number>::size()> get_phi_neighbor_to_dst(unsigned int face) const
+    std::bitset<VectorizedArray<Number>::size()>
+    get_phi_neighbor_to_dst(unsigned int face) const
     {
       return phi_neighbor_to_dst[face];
     }
 
-    std::bitset<VectorizedArray<Number>::size()> get_phi_neighbor_to_fluxmemory(unsigned int face) const
+    std::bitset<VectorizedArray<Number>::size()>
+    get_phi_neighbor_to_fluxmemory(unsigned int face) const
     {
       return phi_neighbor_to_fluxmemory[face];
     }
@@ -132,7 +163,8 @@ namespace HDG_WE
     // vector of length cells with correspondent cluster ids
     std::vector<unsigned int> element_categories;
 
-    // vectors of length cells with indicator for neighbors with smaller/bigger time step size
+    // vectors of length cells with indicator for neighbors with smaller/bigger
+    // time step size
     std::vector<bool> cell_have_faster_neighbor;
     std::vector<bool> cell_have_slower_neighbor;
 
@@ -170,7 +202,7 @@ namespace HDG_WE
     std::vector<unsigned int> cluster_update_order;
 
     // update times (of length updates)
-    mutable std::vector<std::vector<Number> > cluster_update_times;
+    mutable std::vector<std::vector<Number>> cluster_update_times;
 
     // number of updates
     unsigned int n_updates;
@@ -191,7 +223,7 @@ namespace HDG_WE
     mutable std::vector<bool> evaluate_face;
 
     // evaluation times
-    mutable Number t1,t2,te,dt;
+    mutable Number t1, t2, te, dt;
 
     // helper times
     mutable Number t1fa, t2fa, t1sl, t2sl, t1sa, t2sa;
@@ -206,26 +238,31 @@ namespace HDG_WE
     bool use_ader_post;
 
     // masks for each face
-    mutable std::vector<std::bitset<n_vect> > phi_to_dst;
-    mutable std::vector<std::bitset<n_vect> > phi_neighbor_to_dst;
-    mutable std::vector<std::bitset<n_vect> > phi_to_fluxmemory;
-    mutable std::vector<std::bitset<n_vect> > phi_neighbor_to_fluxmemory;
+    mutable std::vector<std::bitset<n_vect>> phi_to_dst;
+    mutable std::vector<std::bitset<n_vect>> phi_neighbor_to_dst;
+    mutable std::vector<std::bitset<n_vect>> phi_to_fluxmemory;
+    mutable std::vector<std::bitset<n_vect>> phi_neighbor_to_fluxmemory;
 
     // helper vectors to avoid the get_cell_iterator in update_elements
-    std::vector<std::vector<std::bitset<n_vect> > > cell_neighbor_has_children; // order is n,e,v
-    std::vector<std::vector<std::vector<int> > > cell_neighbor_index;           // order is n,v,e
-    std::vector<std::vector<std::vector<int> > > cell_neighbor_active_cell_index; // order is n,v,e
+    std::vector<std::vector<std::bitset<n_vect>>>
+      cell_neighbor_has_children; // order is n,e,v
+    std::vector<std::vector<std::vector<int>>>
+      cell_neighbor_index; // order is n,v,e
+    std::vector<std::vector<std::vector<int>>>
+      cell_neighbor_active_cell_index; // order is n,v,e
 
-    std::vector<std::vector<unsigned int> > mf_faceinfo_cellsminus;
-    std::vector<std::vector<unsigned int> > mf_faceinfo_cellsplus;
+    std::vector<std::vector<unsigned int>> mf_faceinfo_cellsminus;
+    std::vector<std::vector<unsigned int>> mf_faceinfo_cellsplus;
 
-    template <typename Operator> void update_elements(const Operator &op,
-                                                      LinearAlgebra::distributed::Vector<Number>        &dst,
-                                                      LinearAlgebra::distributed::Vector<Number>        &local_state,
-                                                      const unsigned int actual_cluster,
-                                                      const bool write_to_fluxmemory = true) const;
+    template <typename Operator>
+    void
+    update_elements(const Operator                             &op,
+                    LinearAlgebra::distributed::Vector<Number> &dst,
+                    LinearAlgebra::distributed::Vector<Number> &local_state,
+                    const unsigned int                          actual_cluster,
+                    const bool write_to_fluxmemory = true) const;
   };
 
-}
+} // namespace HDG_WE
 
 #endif
